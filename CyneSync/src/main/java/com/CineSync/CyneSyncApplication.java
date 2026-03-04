f1b9e129f32f7d9a64305f37908b4cb80f80e414
michaelamaraldev <michaelzerotreze@gmail.com>
2026-03-04 15:03:44 -0300

    spring setup + docker compose

---
+++ CyneSync/src/main/java/com/CineSync/CyneSyncApplication.java
@@ -0,0 +1,13 @@
+package com.CineSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyneSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyneSyncApplication.class, args);
+	}
+
+}
---
+++ CyneSync/src/test/java/com/CineSync/CyneSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CineSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyneSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
